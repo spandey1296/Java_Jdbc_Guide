e9d967f9e610a103387f5c607696ff14c3d981ea
shivant47@gmail.com <shivant47@gmail.com>
2020-10-29 23:07:15 +0530

    new files added

--- src/com/company/ResultSetDemo.java
+++ src/com/company/ResultSetDemo.java
@@ -6,11 +6,7 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
-/**
- *
- * @author spandey1296.github.io
- *
- */
+
 public class ResultSetDemo {
 
 	public static void main(String[] args) throws SQLException {
